d3e1a77247b7521e5033508c5a70f8c3366ec0e8
roger.xia <roger.xialei@gmail.com>
2015-10-28 17:49:56 +0800

    sql

--- src/main/java/com/aug3/test/sparkme/EarningsCalculate.java
+++ src/main/java/com/aug3/test/sparkme/EarningsCalculate.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Column;
 import org.apache.spark.sql.DataFrame;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SQLContext;
@@ -16,10 +17,10 @@ public class EarningsCalculate {
 	private static final Logger LOGGER = Logger.getLogger(EarningsCalculate.class);
 
 	private static final String MYSQL_DRIVER = "com.mysql.jdbc.Driver";
-	private static final String MYSQL_USERNAME = "user";
-	private static final String MYSQL_PWD = "user";
-	private static final String MYSQL_CONNECTION_URL = "jdbc:mysql://192.168.0.3:3306/ada-fd?user=" + MYSQL_USERNAME
-			+ "&password=" + MYSQL_PWD;
+	private static final String MYSQL_USERNAME = "ada_user";
+	private static final String MYSQL_PWD = "ada_user";
+	private static final String MYSQL_CONNECTION_URL = "jdbc:mysql://192.168.250.208:3306/ada-fd?user="
+			+ MYSQL_USERNAME + "&password=" + MYSQL_PWD;
 
 	private static final JavaSparkContext sc = new JavaSparkContext(new SparkConf().setAppName("EarningsCalculate")
 			.setMaster("local[*]"));
@@ -32,11 +33,16 @@ public class EarningsCalculate {
 		Map<String, String> options = new HashMap<>();
 		options.put("driver", MYSQL_DRIVER);
 		options.put("url", MYSQL_CONNECTION_URL);
-		options.put("dbtable", "(select dt,tick,close from hq_price where dt = '2015-10-23') as quotes");
+		// options.put("dbtable",
+		// "(select dt,tick,close from hq_price where dt = '2015-10-23') as quotes");
 		// options.put("partitionColumn", "id");
 		// options.put("lowerBound", "000001");
 		// options.put("upperBound", "999999");
 		// options.put("numPartitions", "10");
+
+		DataFrame dateDF = sqlContext.read().json("trade_date.json");
+
+		dateDF.select(new Column("dt"));
 
 		DataFrame df = sqlContext.read().format("jdbc").options(options).load();
 
